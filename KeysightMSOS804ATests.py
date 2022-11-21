b8af1589b92161751a671faca4b736f749dfb4dd
Benjamin Saggin <benjamin.saggin@live.fr>
2022-11-21 15:17:58 +0100

    [Fix] See preceding commit

--- KeysightMSOS804ATests.py
+++ KeysightMSOS804ATests.py
@@ -3,10 +3,11 @@
 from KeysightMSOS804A import AddFunction, FFTMagnitudeFunction, KeysightMSOS804A
 
 osc = KeysightMSOS804A("TCPIP0::k-msos804a-30101.ies.univ-montp2.fr::inst0::INSTR")
+print(osc)
 osc.Connect()
 osc.Functions[1].ChangeFunction(AddFunction, [osc.Channels[1], osc.Channels[2]])
 channel1 = next((channel for channel in osc.Channels if channel.Address == 1), None)
-#print(osc)
+#
 # print(channel1.GetMaximum())
 # print(channel1.GetMinimum())
 # print(channel1.GetAverage())
