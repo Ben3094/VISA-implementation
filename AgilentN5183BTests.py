a9b63608b1af69df47749dcbc379c3ceb8f4c4bb
Benjamin Saggin <benjamin.saggin@live.fr>
2023-02-14 18:01:03 +0100

    [Add] Keysight N5183B sweep management [Add] Keysight N5183B frequency multiplier support

--- AgilentN5183BTests.py
+++ AgilentN5183BTests.py
@@ -1,6 +1,8 @@
-from pyVirtualLab.Instruments.AgilentN5183B import AgilentN5183B, OutSignal
+from pyVirtualLab.Instruments.AgilentN5183B import AgilentN5183B, OutSignal, TriggerSource
+from pandas import read_csv
+from io import StringIO
 
-scr = AgilentN5183B('TCPIP0::169.254.189.231::inst0::INSTR')
+scr = AgilentN5183B('TCPIP::A-N5183B-250181.ies.univ-montp2.fr::inst0::INSTR')
 scr.Connect()
 print(scr.SweepOutSignal)
 print(scr.Tigger1OutSignal)
@@ -16,3 +18,30 @@ scr.IsLowFrequencyOutputEnabled = False
 print(scr.IsLowFrequencyOutputEnabled)
 scr.IsLowFrequencyOutputEnabled = True
 print(scr.IsLowFrequencyOutputEnabled)
+
+scr.SweepTriggerSource = TriggerSource.Trigger1
+scr.SweepPointTriggerSource = TriggerSource.Immediate
+sweepPoints = """Frequency,Power,DwellTime
+36000000000, -20, 0.580
+36000000000, -10, 0.005
+36000000000, -20, 0.005
+36000000000, -10, 0.005
+36000000000, -20, 0.005
+36000000000, -10, 0.005
+36000000000, -20, 0.005
+36000000000, -10, 0.005
+36000000000, -20, 0.005
+36000000000, -10, 0.005
+36000000000, -20, 0.005
+36000000000, -10, 0.005
+36000000000, -20, 0.005
+36000000000, -10, 0.005
+36000000000, -20, 0.005
+"""
+sweepPoints = read_csv(StringIO(sweepPoints))
+scr.SweepPoints = [(frequency, power, dwellTime) for frequency, power, dwellTime in zip(sweepPoints['Frequency'], sweepPoints['Power'], sweepPoints['DwellTime'])]
+print(f"Sweep points: {scr.SweepPoints}")
+scr.IsSweepReversed = True
+print(f"Sweep{'' if scr.IsSweepReversed else ' not'} reversed")
+scr.IsSweepReversed = False
+print(f"Sweep{'' if scr.IsSweepReversed else ' not'} reversed")
