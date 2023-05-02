53605fe969ccf958683f73df8b995fcf190618b5
Benjamin Saggin <benjamin.saggin@live.fr>
2023-05-02 18:24:37 +0200

    [Fix] Keysight MSOS804A waveform memory channel support in StringToChannel method [Add] Keysight MSOS804A measurements over other channel type than analogs [Fix] Keysight MSOS804A subtract function name typo

--- KeysightMSOS804ATests.py
+++ KeysightMSOS804ATests.py
@@ -1,10 +1,20 @@
-from pyVirtualLab.Instruments.KeysightMSOS804A import AddFunction, FFTMagnitudeFunction, KeysightMSOS804A
+from pyVirtualLab.Instruments.KeysightMSOS804A import AddFunction, FFTMagnitudeFunction, KeysightMSOS804A, SubtractFunction
 
 osc = KeysightMSOS804A("TCPIP0::k-msos804a-30101.ies.univ-montp2.fr::inst0::INSTR")
 print(osc)
 osc.Connect()
+osc.AnalogChannels[1].IsEnabled = True
 osc.Functions[1].ChangeFunction(AddFunction)
 osc.Functions[1].Operand1 = osc.AnalogChannels[3]
+osc.WaveformMemoryChannels[1].Save(osc.AnalogChannels[1])
+osc.WaveformMemoryChannels[1].IsEnabled = True
+subtractFunction:SubtractFunction = osc.Functions[2]
+subtractFunction.ChangeFunction(SubtractFunction)
+subtractFunction.FirstOperand = osc.AnalogChannels[1]
+subtractFunction.SecondOperand = osc.WaveformMemoryChannels[1]
+subtractFunction.IsEnabled = True
+print(subtractFunction.GetArea())
+osc.AnalogChannels[1].IsEnabled = False
 channel1 = osc.AnalogChannels[1]
 channel1.Offset = 1
 channel1.Scale = 0.5
