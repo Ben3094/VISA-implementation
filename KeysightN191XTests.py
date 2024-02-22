f1facc6edc55970390d9d01eaa29db493e9f5251
Benjamin SAGGIN <benjamin.saggin@live.fr>
2024-02-22 19:24:12 +0100

    [Fix] Clean Keysight N191X calibration factor set management

--- KeysightN191XTests.py
+++ KeysightN191XTests.py
@@ -1,10 +1,8 @@
-from pyVirtualLab.Instruments.KeysightN191X import KeysightN191X, CalibrationFactorsSet
+from pyVirtualLab.Instruments.KeysightN191X import KeysightN191X
 import pandas
 
 powerMeter = KeysightN191X('TCPIP0::k-n1913a-80021.ies.univ-montp2.fr::inst0::INSTR')
 powerMeter.Connect()
-powerMeter.CalibrationFactorsSets['V8486A'] = CalibrationFactorsSet(powerMeter, 'V8486A')
 cal = pandas.read_csv("V8486A calibration.csv")
 cal = dict(zip(cal['Frequency (Hz)'], cal['Calibration factor (%)']))
-powerMeter.CalibrationFactorsSets['CUSTOM_0'].CalibrationFactors = cal
-powerMeter.CalibrationFactorsSets['CUSTOM_0'].Name = 'V8486A'
+powerMeter.CalibrationFactorsSets['V8486B'] = cal
