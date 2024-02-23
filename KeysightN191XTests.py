04b3f6ef2449c50f61609e9e929a2710bdf275aa
Benjamin SAGGIN <benjamin.saggin@live.fr>
2024-02-23 11:08:39 +0100

    [Fix] Keysight N191X embedded calibration factors sets

--- KeysightN191XTests.py
+++ KeysightN191XTests.py
@@ -6,3 +6,8 @@ powerMeter.Connect()
 cal = pandas.read_csv("V8486A calibration.csv")
 cal = dict(zip(cal['Frequency (Hz)'], cal['Calibration factor (%)']))
 powerMeter.CalibrationFactorsSets['V8486B'] = cal
+sensor = powerMeter.Sensors[1]
+sensor.AssociatedCalibrationFactorsSetName = 'V8486B'
+for freq in cal.keys():
+	sensor.Frequency = freq
+	print(sensor.Power)
